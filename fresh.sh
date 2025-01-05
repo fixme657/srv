b2f2b0e736df809882fe65ebb395923ad415e2c1
fixme657 <fixme657@gmail.com>
2025-01-05 18:51:39 +0200

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -16,6 +16,22 @@ sudo apt-get install -y \
 # Check the status of installation
 if [ $? -eq 0 ]; then
     echo "All packages installed successfully."
+# Function to get local IP
+echo "find your ip"
+get_local_ip() {
+    local_ip=$(hostname -I | awk '{print $1}')
+    echo "Local IP: $local_ip"
+}
+
+# Function to get WAN IP
+get_wan_ip() {
+    wan_ip=$(curl -s ifconfig.me)
+    echo "WAN IP: $wan_ip"
+}
+
+# Display the IPs
+get_local_ip
+get_wan_ip
 else
     echo "Error occurred during package installation." >&2
     exit 1
